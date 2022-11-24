5ee206532ef99575b46056600c32afb6263b7ac2
yechan <baesku789@gmail.com>
2022-11-24 14:22:11 +0900

    feat: - create-user.dto.ts name과 비밀번호가 같을경우 transform으로 처리

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -5,8 +5,16 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { BadRequestException } from '@nestjs/common';
 
 export class CreateUserDto {
+  @Transform(({ value, obj }) => {
+    if (obj.password.includes(obj.name.trim())) {
+      throw new BadRequestException('password는 name과 일치할 수 없습니다.');
+    }
+    return value.trim();
+  })
   @IsString()
   @MinLength(2)
   @MaxLength(30)
