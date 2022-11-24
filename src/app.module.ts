a8e5e86679261c342ead21103fb2eaa7ae3238ff
yechan <baesku789@gmail.com>
2022-11-24 15:43:15 +0900

    feat: - TypeOrmModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
 import emailConfig from './config/emailConfig';
 import { validationSchema } from './config/validationSchema';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -16,6 +17,16 @@ import { validationSchema } from './config/validationSchema';
       load: [emailConfig],
       isGlobal: true,
       validationSchema,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'test',
+      entities: [__dirname + '/**/*/entity{.ts,.js}'],
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
     }),
   ],
   controllers: [AppController],
