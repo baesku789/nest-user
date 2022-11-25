4ec9b18b8ddc75e700babee2f7b881a686e275dc
yechan <baesku789@gmail.com>
2022-11-25 16:59:57 +0900

    feat: users layer 추가

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,13 @@
+export class User {
+  constructor(
+    private id: string,
+    private name: string,
+    private email: string,
+    private password: string,
+    private signupVerifyToken: string,
+  ) {}
+}
+
 export class UserInfo {
   id: string;
   name: string;
